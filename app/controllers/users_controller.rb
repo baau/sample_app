8451ee3de9ed05b9968bb19b6c223c82ea180ca0
Bharat Singhal <soc.bharat@gmail.com>
2014-06-18 23:08:31 +0530

    Added a stub user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
   def new
   	@title = "Sign In"
+
+  end
+
+  def show
+  	@user = User.find(params[:id]) 
+  end 
+
+  def create
+    User.create(user_params)
+  end
+
+  private
+
+  def person_params
+    params.require(:Users).permit(:name, :email)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
+# Table name : User
+
+# id			: integer 		not null,priamry key
+# name 		: string(255)
+# email		: string(255)
+# created_at 	: datetime
+# updated_at   : datetime
+
+
+
+
 class User < ActiveRecord::Base
 	# this shows that only these "name","email" columns are editable by the
-	# end user 
-	attr_accessible :name,:email
+	# end user
+	email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+	validates :name,:presence => true,
+			  :length => {:maximum => 50}
+	validates :email,:presence => true,
+			  :format => {:with => email_regex},
+			  :uniqueness => {:case_sensitive => false}
 end
